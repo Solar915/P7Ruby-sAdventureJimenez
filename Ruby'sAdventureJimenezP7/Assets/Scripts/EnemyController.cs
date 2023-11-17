705c5c259e7bcbb50472af95effc56b8084cded5
Ethan Jimenez <723068@student.dusd.net>
2023-11-17 15:26:46 -0800

    I have created an animator for both the robot and Ruby and have made the robot a prefab
    For Ruby's Adventure I have created animations and a animator for both the robot and Ruby so they could walk and run around the world.

--- Ruby'sAdventureJimenezP7/Assets/Scripts/EnemyController.cs
+++ Ruby'sAdventureJimenezP7/Assets/Scripts/EnemyController.cs
@@ -14,11 +14,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class EnemyController : MonoBehaviour
     {
 
         timer -= Time.deltaTime;
-        if(timer < 0)
+        if (timer < 0)
         {
             direction = -direction;
             timer = changeTime;
@@ -34,18 +37,30 @@ public class EnemyController : MonoBehaviour
 
     }
     void FixedUpdate()
-    { 
+    {
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
+            animator.SetFloat("MoveX", 0);
+            animator.SetFloat("MoveY", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("MoveX", direction);
+            animator.SetFloat("MoveY", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
-    
-            rigidbody2d.MovePosition(position);
-        }  
+
+        rigidbody2d.MovePosition(position);
     }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+        if(player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
 
