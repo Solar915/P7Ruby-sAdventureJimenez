c15abac6100affcb5f0c78af2f865c54ea692da1
Ethan Jimenez <723068@student.dusd.net>
2023-11-26 17:00:13 -0800

    I have created a particle system for the robots and now working on UI
    On Ruby's Adventure, I have made a particle system for my robots and I'm starting to work on UI

--- Ruby'sAdventureJimenezP7/Assets/Scripts/EnemyController.cs
+++ Ruby'sAdventureJimenezP7/Assets/Scripts/EnemyController.cs
@@ -9,6 +9,7 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 2.0f;
 
+    public ParticleSystem smokeEffect;
 
     Rigidbody2D rigidbody2d;
 
@@ -78,6 +79,7 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
 
--- Ruby'sAdventureJimenezP7/Assets/Scripts/Projectile.cs
+++ Ruby'sAdventureJimenezP7/Assets/Scripts/Projectile.cs
@@ -29,7 +29,7 @@ public class Projectile : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         EnemyController e = other.collider.GetComponent<EnemyController>();
-        if (e != null) { }
+        if (e != null) 
         {
             e.Fix();
         }
